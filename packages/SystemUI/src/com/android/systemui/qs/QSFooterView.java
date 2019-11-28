9f796e24fb9302cb8d61b92a164c9cf2a0f82357
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-28 14:15:00 +0300

    SystemUI: Allow customizing footer text [1/2]
    Change-Id: I08b080914dc8041776eaf88333f338d505ed9f88

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterView.java
@@ -84,22 +84,22 @@ public class QSFooterView extends FrameLayout {
 
     private void setBuildText() {
         if (mBuildText == null) return;
-        boolean showFooterText = Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.FOOTER_TEXT_SHOW, 0,
+        boolean isShow = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.QS_FOOTER_TEXT_SHOW, 0,
                         UserHandle.USER_CURRENT) == 1;
-        if (showFooterText) {
-            mBuildText.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE_OR_CODENAME,
-                    Build.ID));
-            // Set as selected for marquee before its made visible, then it won't be announced when
-            // it's made visible.
-            mBuildText.setSelected(true);
-            mShouldShowBuildText = true;
+        String text = Settings.System.getStringForUser(mContext.getContentResolver(),
+                        Settings.System.QS_FOOTER_TEXT_STRING,
+                        UserHandle.USER_CURRENT);
+        if (isShow) {
+            if (text == null || text.isEmpty()) {
+                mBuildText.setText("#BringTheShishuBack");
+                mBuildText.setVisibility(View.VISIBLE);
+            } else {
+                mBuildText.setText(text);
+                mBuildText.setVisibility(View.VISIBLE);
+            }
         } else {
-            mBuildText.setText(null);
-            mShouldShowBuildText = false;
-            mBuildText.setSelected(false);
+            mBuildText.setVisibility(View.GONE);
         }
     }
 
