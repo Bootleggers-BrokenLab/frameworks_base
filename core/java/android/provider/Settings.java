7cc125686b509e1fbc75e8c4bb363520b3c66995
AnkitGourav <Ankitgourav035@gmail.com>
2020-01-11 23:26:48 +0530

    SystemUI: Make header image height offset user configurable [1/2]
    Bootleggers edit: Parse the int directly and then convert it to dp
    
    Change-Id: I28d5cae27ad44d2a12bec78a81319024674a6d74

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6224,6 +6224,12 @@ public final class Settings {
         public static final String STATUS_BAR_FILE_HEADER_IMAGE = "status_bar_file_header_image";
 
         /**
+         * Header height
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_HEIGHT = "status_bar_custom_header_height";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6451,6 +6457,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_PROVIDER);
             PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_IMAGE);
             PRIVATE_SETTINGS.add(STATUS_BAR_FILE_HEADER_IMAGE);
+            PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_HEIGHT);
         }
 
         /**
