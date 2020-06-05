26ee9404b9f54bbc3bc0270db712a78873bdc71c
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-05 22:21:04 +0300

    base: Allow choosing a custom vibration pattern [1/3]
    Change-Id: I0d00f94b185a1032deb3e8413720d47a411cd6f7

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -109,5 +109,6 @@ public class SystemSettings {
         Settings.System.NOTIFICATION_COOLDOWN_VIBRATE_UNLOCKED,
         Settings.System.FULLSCREEN_GESTURES,
         Settings.System.RINGTONE_VIBRATION_PATTERN,
+        Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
     };
 }
