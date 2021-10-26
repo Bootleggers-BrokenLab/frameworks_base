a97e1b8ec1598ca4d0e0ba2af94bb1795e0b498c
Danny Lin <danny@kdrag0n.dev>
2021-10-25 19:32:33 -0700

    SystemUI: Allow Wi-Fi/cell tiles to co-exist with provider model
    The dedicated Wi-Fi/cellular data QS tiles are partially working now
    that we've exposed them in the list of tile options, but they're very
    buggy and somewhat broken when the provider model (unified internet) is
    enabled.
    
    Allow the tiles to co-exist with InternetTile and the provider model
    (including both settings_provider_model and combined signal icons) by
    always including QS icons in Wi-Fi/data indicator callbacks, and add
    a new flag to indicate whether they should be shown in the internet
    tile.
    
    (cdesai, mkbestas: Port to 12L)
    
    Change-Id: I10af134b35dfabdb9275f1aca8ca8512e1db6d27

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -324,7 +324,8 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
                 qsInfo.description,
                 mSubscriptionInfo.getSubscriptionId(),
                 mCurrentState.roaming,
-                sbInfo.showTriangle);
+                sbInfo.showTriangle,
+                mCurrentState.isDefault);
         callback.setMobileDataIndicators(mobileDataIndicators);
     }
 
@@ -334,8 +335,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         CharSequence qsDescription = null;
 
         if (mCurrentState.dataSim) {
-            // only show QS icons if the state is also default
-            if (!mCurrentState.isDefault) {
+            if (mProviderModelBehavior) {
                 return new QsInfo(qsTypeIcon, qsIcon, qsDescription);
             }
 
