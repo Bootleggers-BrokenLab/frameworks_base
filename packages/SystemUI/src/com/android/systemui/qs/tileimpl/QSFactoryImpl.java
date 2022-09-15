2a1b8bd70636d94ef27a3ea38470dd38f25eece1
Semavi Ulusoy <doc.divxm@gmail.com>
2022-09-16 01:09:03 +0300

    base: WeatherTile: remove till it's fixed
    frameworks/base/packages/SystemUI/src/com/android/systemui/qs/tiles/WeatherTile.
    java:56: error: symbol not found com.android.systemui.plugins.qs.DetailAdapter
    
    import com.android.systemui.plugins.qs.DetailAdapter;
    
    DetailAdapter.java was removed here: https://github.com/AICP/frameworks_base/commit/c292eff31f6b99a7d48caa1efb49800080d641d3
    
    Change-Id: Ie78d7ed5d6eff9797bafb00a9817bdf67a6ccecb

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -55,7 +55,6 @@ import com.android.systemui.qs.tiles.ReduceBrightColorsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
-import com.android.systemui.qs.tiles.WeatherTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.qs.tiles.WorkModeTile;
 import com.android.systemui.util.leak.GarbageMonitor;
@@ -101,7 +100,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<OneHandedModeTile> mOneHandedModeTileProvider;
     private final Provider<DreamTile> mDreamTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
-    private final Provider<WeatherTile> mWeatherTileProvider;
 
     private final Lazy<QSHost> mQsHostLazy;
     private final Provider<CustomTile.Builder> mCustomTileBuilderProvider;
@@ -140,8 +138,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<OneHandedModeTile> oneHandedModeTileProvider,
             Provider<ColorCorrectionTile> colorCorrectionTileProvider,
             Provider<DreamTile> dreamTileProvider,
-            Provider<CaffeineTile> caffeineTileProvider,
-            Provider<WeatherTile> weatherTileProvider) {
+            Provider<CaffeineTile> caffeineTileProvider) {
         mQsHostLazy = qsHostLazy;
         mCustomTileBuilderProvider = customTileBuilderProvider;
 
@@ -176,7 +173,6 @@ public class QSFactoryImpl implements QSFactory {
         mColorCorrectionTileProvider = colorCorrectionTileProvider;
         mDreamTileProvider = dreamTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
-        mWeatherTileProvider = weatherTileProvider;
     }
 
     /** Creates a tile with a type based on {@code tileSpec} */
@@ -254,8 +250,6 @@ public class QSFactoryImpl implements QSFactory {
                 return mDreamTileProvider.get();
             case "caffeine":
                 return mCaffeineTileProvider.get();
-            case "weather":
-                return mWeatherTileProvider.get();
         }
 
         // Custom tiles
