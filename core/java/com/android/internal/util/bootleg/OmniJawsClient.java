5bcc1d3743950fc600924046a1f5fbfe65fc05d4
tribual <tribual@web.de>
2023-12-05 16:05:40 +0100

    base: OmniJaws: adapt for Bootleggers

--- core/java/com/android/internal/util/bootleg/OmniJawsClient.java
+++ core/java/com/android/internal/util/bootleg/OmniJawsClient.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.android.internal.util.evolution;
+package com.android.internal.util.bootleg;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
